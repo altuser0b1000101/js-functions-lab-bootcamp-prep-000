c3e186b697a9ccf09b5ee745bf2e1c9af07421c9
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-10 00:26:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name) {
 function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
-function holidayCountdown(holiday,number) {
-  return 
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
 }
